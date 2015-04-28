d2bd6534ea89a7ef48023c05fed6b3176f988044
Joe Stein <joeaarons@gmail.com>
2015-04-27 23:14:12 -0400

    Live polling, dummy data

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, './views', 'index.html'));
 });
 
+app.get('/search', function(req, res) {
+  console.log(req.query);
+  res.send({'results': [
+    {'title': 'A Thing', 'rating': 80, 'available': true, 'plot': 'asdf', 'genres': ['Comedy', 'Action', 'Romance']},
+    {'title': 'Not Available', 'rating': 34, 'available': false, 'genres': ['Adventure', 'Thriller']}
+  ]});
+});
+
 var server = app.listen(process.env.PORT || 3000, function() {
   var addr = server.address();
   console.log("Listening at %s:%s", addr.address, addr.port);

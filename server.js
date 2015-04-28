48651e4e83ede4318ff5c4fbb723212bd445a70a
Joe Stein <joeaarons@gmail.com>
2015-04-27 22:01:45 -0400

    Moar styling and markup

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
+app.use(express.static('static'));
+
 app.get('/', function(req, res) {
-  res.send('Hello world!');
+  res.sendFile(path.join(__dirname, './views', 'index.html'));
 });
 
-var server = app.listen(8000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
   var addr = server.address();
   console.log("Listening at %s:%s", addr.address, addr.port);
 });
